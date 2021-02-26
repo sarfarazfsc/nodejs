580ac4528439af184e602d7706c8648522f84b38
Sarfaraz <mdsarfarazalam097@gmail.com>
2021-02-26 17:31:00 +0530

    Learn to fetch a file using node with url

---
+++ demo_fileserver.js
@@ -0,0 +1,22 @@
+
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+http.createServer(function(req,res){
+
+     const q = url.parse(req.url,true);
+     const filename = '.'+q.pathname;
+     
+     fs.readFile(filename,function(err,data){
+      
+           if(err) {
+               res.writeHead(404,{'Content-Type':'text/html'});
+               return res.end('404 not found');
+           }
+
+           res.writeHead(200,{'Content-Type':'text/html'});
+           res.write(data);
+           return res.end();
+     });
+}).listen(8080);
