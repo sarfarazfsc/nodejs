d413196e5d727af5ba956e3aef92864052de02fe
Sarfaraz <mdsarfarazalam097@gmail.com>
2021-03-04 16:34:24 +0530

    Learn how to upload external file

---
+++ demo_uploadFile.js
@@ -0,0 +1,31 @@
+
+const http = require('http');
+const formidable = require('formidable');
+const fs = require('fs');
+
+http.createServer(function(req, res){
+ 
+   if(req.url == '/fileupload'){
+
+    var form = new formidable.IncomingForm();  
+    form.parse(req, function(err, fields, files){
+
+        var oldpath = files.filetoupload.path;
+        var newpath =  '/home/sarfu/Test/Node/nodejs/File/'+files.filetoupload.name;
+        fs.rename(oldpath, newpath, function(err){
+            if(err) throw err;
+            res.write('Files upload successfull!');
+            res.end();
+        })
+    })
+
+   }else{
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.write("<form action = 'fileupload' method = 'post' enctype = 'multipart/form-data'>");
+        res.write("<input type ='file' name ='filetoupload'><br><br>");
+        res.write("<input type = 'submit'></form> ");
+    return res.end();
+
+   }
+    
+}).listen(8080);
